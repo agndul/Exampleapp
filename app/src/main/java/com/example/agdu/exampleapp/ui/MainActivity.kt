62b89d42efc4f7e93aaa6775f606ef6231d867cf
Agnieszka Dulęba <agdu@ailleron.com>
2017-04-20 15:10:47 +0200

    Add rxbinding to button

--- app/src/main/java/com/example/agdu/exampleapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/agdu/exampleapp/ui/MainActivity.kt
@@ -9,11 +9,13 @@ import com.example.agdu.exampleapp.api.RestApi.Companion.retrofitInstance
 import com.example.agdu.exampleapp.api.pojo.ExampleResponse
 import com.example.agdu.exampleapp.api.pojo.Item
 import com.example.agdu.exampleapp.api.pojo.StackService
+import com.jakewharton.rxbinding2.view.RxView
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
+import java.util.concurrent.TimeUnit
+
 
 class MainActivity : AppCompatActivity() {
     val rvAdapter = RvAdapter()
@@ -27,24 +29,35 @@ class MainActivity : AppCompatActivity() {
         val stackService = retrofitInstance().create(StackService::class.java)
         val stackResponse: Observable<ExampleResponse> = stackService.getAnswers()
 
-        stackResponse
-                .subscribeOn(Schedulers.newThread())
-                .concatMap { items -> Observable.fromArray(items.items) }
-                .map{it ->
-                    var sorted: List<Item>? = it
-               //    Collections.sort(sorted, Item.DisplayNameComparator())
-                    sorted = sorted!!.sortedWith(Item.DisplayNameComparator())
-                    sorted
-                }
+        RxView.clicks(btn_click)
+                .debounce(1000, TimeUnit.MILLISECONDS)
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe {
-                    it!!.forEach {
-                        rvAdapter.addSingleElement(it.owner!!.displayName!!)
-                        Log.e("Changed displayname", it.owner?.displayName)
-                    }
-               }
+                .subscribe{
+                    btn_click.text = "Loading.. "
+                    stackResponse
+                            .subscribeOn(Schedulers.newThread())
+                            .concatMap { items -> Observable.fromArray(items.items) }
+                            .map { it ->
+                                var sorted: List<Item>? = it
+                                sorted = sorted!!.sortedWith(Item.DisplayNameComparator())
+                                sorted
+                            }
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe( {
+                                it!!.forEach {
+                                    rvAdapter.addSingleElement(it.owner!!.displayName!!)
+                                    Log.e("Changed displayname", it.owner?.displayName)
+                                }
+                            }, {
+                                btn_click.text = "Error.. "
+                            }, {
+                                btn_click.text = "Finished.. "
+                            } )
 
+                    }
     }
+
+
 
     fun initRv(): Unit{
         rv_list.apply {
